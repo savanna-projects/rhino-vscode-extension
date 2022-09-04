b7596609e2b637d57b066f0f17a279b8c8bab510
Roei Sabag <gravity.api@outlook.com>
2022-09-04 16:06:10 +0300

    updates

--- src/commands/create-project.ts
+++ src/commands/create-project.ts
@@ -71,6 +71,7 @@ export class CreateProjectCommand extends Command {
             CreateProjectCommand.createSampleScripts(folderUri);
             CreateProjectCommand.createSamplePipelines(folderUri);
             CreateProjectCommand.createSampleModels(folderUri);
+            CreateProjectCommand.createSampleEnvironment(folderUri);
             // user switch ends
             CreateProjectCommand.openFolder(folderUri);
         });
@@ -152,7 +153,7 @@ export class CreateProjectCommand extends Command {
             "",
             "[test-expected-results]",
             "[1] verify that {url} match {google}"
-        ]
+        ];
         let bodyWithModels = [
             "/**┌─[ General Information ]──────────────────────────────────────────────────────────────────────",
             "/**│",
@@ -188,14 +189,85 @@ export class CreateProjectCommand extends Command {
             "",
             "[test-expected-results]",
             "[1] verify that {url} match {google}"
+        ];
+        let bodyWithEnvironment = [
+            "/**┌─[ General Information ]──────────────────────────────────────────────────────────────────────",
+            "/**│",
+            "/**│ Connect & Invoke Test Case",
+            "/**│ ==========================",
+            "/**│ 1. Use [Ctrl]+[Shift]+[P] to bring up the commands palette.",
+            "/**│ 2. Type 'Rhino' to filter out all 'Rhino' commands.",
+            "/**│ 3. Click on the command 'Rhino: Connect to Rhino, fetch Metadata & activate commands'.",
+            "/**│ 4. Use [Ctrl]+[Shift]+[P] to bring up the commands palette.",
+            "/**│ 4. Type 'Rhino' to filter out all 'Rhino' commands.",
+            "/**│ 5. Click on the command 'Rhino: Runs the automation test(s) from the currently open document'.",
+            "/**│",
+            "/**│ View Documentation",
+            "/**│ ==================",
+            "/**│ 1. Right-Click to bring up the context menu.",
+            "/**│ 2. Click on 'Rhino: Show Documentation' command.",
+            "/**│",
+            "/**└──────────────────────────────────────────────────────────────────────────────────────────────",
+            "/**",
+            "[test-id]         EXAMPLE-02",
+            "[test-scenario]   verify that results can be retrieved when searching by any keyword",
+            "[test-categories] Sanity, Ui, Search",
+            "[test-priority]   1 - critical",
+            "[test-severity]   1 - critical",
+            "[test-tolerance]  0%",
+            "",
+            "[test-actions]",
+            "1. go to url {{$getparam --name:ApplicationUrl}}",
+            "2. send keys {automation is fun} into {search text-box}",
+            "3. click on the first {auto-complete item}",
+            "4. wait {1500}",
+            "5. close browser",
+            "",
+            "[test-expected-results]",
+            "[1] verify that {url} match {google}"
+        ];
+        let bodyWithPlugins = [
+            "/**┌─[ General Information ]──────────────────────────────────────────────────────────────────────",
+            "/**│",
+            "/**│ Connect & Invoke Test Case",
+            "/**│ ==========================",
+            "/**│ 1. Use [Ctrl]+[Shift]+[P] to bring up the commands palette.",
+            "/**│ 2. Type 'Rhino' to filter out all 'Rhino' commands.",
+            "/**│ 3. Click on the command 'Rhino: Connect to Rhino, fetch Metadata & activate commands'.",
+            "/**│ 4. Use [Ctrl]+[Shift]+[P] to bring up the commands palette.",
+            "/**│ 4. Type 'Rhino' to filter out all 'Rhino' commands.",
+            "/**│ 5. Click on the command 'Rhino: Runs the automation test(s) from the currently open document'.",
+            "/**│",
+            "/**│ View Documentation",
+            "/**│ ==================",
+            "/**│ 1. Right-Click to bring up the context menu.",
+            "/**│ 2. Click on 'Rhino: Show Documentation' command.",
+            "/**│",
+            "/**└──────────────────────────────────────────────────────────────────────────────────────────────",
+            "/**",
+            "[test-id]         EXAMPLE-03",
+            "[test-scenario]   verify that results can be retrieved when searching by any keyword",
+            "[test-categories] Sanity, Ui, Search",
+            "[test-priority]   1 - critical",
+            "[test-severity]   1 - critical",
+            "[test-tolerance]  0%",
+            "",
+            "[test-actions]",
+            "1. go to url {{$getparam --name:ApplicationUrl}}",
+            "2. google search {automation is fun}",
+            "3. close browser"
         ]
         let contentBasic = bodyBasic.join('\n');
         let contentWithModels = bodyWithModels.join('\n');
+        let contentWithEnvironment = bodyWithEnvironment.join('\n');
+        let contentWithPlugins = bodyWithPlugins.join('\n');
         let path = ph.join(this.getPath(userPath), 'src', 'Tests', 'Examples');
 
         // write
         this.writeFile(path, 'FindSomethingOnGoogle.rhino', contentBasic);
         this.writeFile(path, 'FindSomethingOnGoogleWithModels.rhino', contentWithModels);
+        this.writeFile(path, 'FindSomethingOnGoogleWithEnvironment.rhino', contentWithEnvironment);
+        this.writeFile(path, 'FindSomethingOnGoogleWithPlugins.rhino', contentWithPlugins);
     }
 
     private static createSamplePlugins(userPath: any) {
@@ -536,6 +608,20 @@ export class CreateProjectCommand extends Command {
         // write
         this.writeFile(pathMarkdown, 'GoogleSearchHomePage.rmodel', contentMarkdown);
         this.writeFile(pathJson, 'GoogleSearchHomePage.json', contentJson);
+    }
+
+    private static createSampleEnvironment(userPath: any) {
+        // setup
+        let body = {
+            "ApplicationUrl": "https://www.google.com"
+        };
+
+        // set content
+        let content = JSON.stringify(body, null, 4);
+        let path = ph.join(this.getPath(userPath), 'src', 'Environments');
+
+        // write
+        this.writeFile(path, 'GoogleSearchEnvironment.json', content);
     }
 
     // open a folder in VS Code workspace
