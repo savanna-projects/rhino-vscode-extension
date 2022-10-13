8d2cbc2073a167b88a497c77e9968a8032ff9687
Gravity API <gravity.api@outlook.com>
2022-10-13 19:46:41 +0300

    optimize documents provider

---
+++ src/contracts/tree-item.ts
@@ -0,0 +1,27 @@
+/*
+ * CHANGE LOG - keep only last 5 threads
+ * 
+ * RESOURCES
+ */
+import * as vscode from 'vscode';
+
+export class TreeItem extends vscode.TreeItem {
+    children: TreeItem[] | undefined;
+    command?: vscode.Command | undefined;
+
+    constructor(
+        label: string,
+        iconPath?: string | vscode.Uri | { light: string | vscode.Uri; dark: string | vscode.Uri } | vscode.ThemeIcon,
+        children?: TreeItem[] | undefined,
+        command?: vscode.Command) {
+
+        super(
+            label,
+            children === undefined
+                ? vscode.TreeItemCollapsibleState.None
+                : vscode.TreeItemCollapsibleState.Expanded);
+        this.children = children;
+        this.command = command;
+        this.iconPath = iconPath;
+    }
+}
