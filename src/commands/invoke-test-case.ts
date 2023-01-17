36a3d3f3ec2d91930a346ff8267eecbcd0afca7d
Ofer Lifshes <ofer.lifshes@finastra.com>
2023-01-17 09:34:45 +0200

    extracted displayRunLog function from invoke

--- src/commands/invoke-test-case.ts
+++ src/commands/invoke-test-case.ts
@@ -131,43 +131,8 @@ export class InvokeTestCaseCommand extends Command {
         var runEnded = false;
         var stopCondition = () => runEnded;
 
-        const displayRunLog = async () => {
-            this.createLogger();
-            if(!this.testRunLogger){
-                throw new Error(`No test run logger created!`);
-            }
-            let logger = this.testRunLogger;
-            logger.show();
-            
-            let logParser = new ServerLogService(this.getRhinoClient());
-            let numberOfLines = 200;
-            let latestLogId = await logParser.getLatestLogId();
-            let runStartTime = new Date();
-            let isAfterRunStart = false;
-
-            let logging = async () => {
-                let log = await logParser.getLog(latestLogId, numberOfLines);
-                let messagesToLog = logParser.parseLog(log ?? "");
-                for(let message of messagesToLog){
-                    if(!isAfterRunStart){
-                        let logDate = ServerLogParser.parseLogTimestamp(message);
-                        isAfterRunStart = logDate > runStartTime
-                    }
-
-                    if(isAfterRunStart){
-                        logger.append(message);
-                    }
-                    
-                    //Wait to slightly stagger writing of logs to channel, allowing easier reading of log continuously.
-                    await Utilities.wait(100);
-                }
-            };
-            
-            Utilities.poll(logging, stopCondition, 1000). then(() => logger.appendLine(`${Utilities.getTimestamp()} - Test run ended.`));
-        }
-
         if(this.loggerConfig?.enableClientSideLogging){
-            displayRunLog();
+            this.displayRunLog(stopCondition, 1000);
         }
         
         // invoke
@@ -189,7 +154,45 @@ export class InvokeTestCaseCommand extends Command {
             }
         });
     }
+    /**
+     * 
+     * @param stopCondition The condition after which
+     * @param interval Interval, in milliseconds, to get the log. Default is 1000ms
+     */
+    private async displayRunLog(stopCondition: (...args: any) => boolean, interval?: number): Promise<void>{
+        this.createLogger();
+        if(!this.testRunLogger){
+            throw new Error(`No test run logger created!`);
+        }
+        let logger = this.testRunLogger;
+        logger.show();
+        
+        let logParser = new ServerLogService(this.getRhinoClient());
+        let numberOfLines = 200;
+        let latestLogId = await logParser.getLatestLogId();
+        let runStartTime = new Date();
+        let isAfterRunStart = false;
 
+        let logging = async () => {
+            let log = await logParser.getLog(latestLogId, numberOfLines);
+            let messagesToLog = logParser.parseLog(log ?? "");
+            for(let message of messagesToLog){
+                if(!isAfterRunStart){
+                    let logDate = ServerLogParser.parseLogTimestamp(message);
+                    isAfterRunStart = logDate > runStartTime
+                }
+
+                if(isAfterRunStart){
+                    logger.append(message);
+                }
+                
+                //Wait to slightly stagger writing of logs to channel, allowing easier reading of log continuously.
+                await Utilities.wait(100);
+            }
+        };
+        
+        Utilities.poll(logging, stopCondition, interval ?? 1000). then(() => logger.appendLine(`${Utilities.getTimestamp()} - Test run ended.`));
+    }
     // creates default configuration with text connector
     private getConfiguration() {
         // setup
