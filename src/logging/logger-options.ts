db1b37bb15351bf708aed2cac70bf12c9fca0df8
Ofer Lifshes <ofer.lifshes@finastra.com>
2023-01-18 08:45:15 +0200

    removed case sensitivity from LoggerOption's loglevel

--- src/logging/logger-options.ts
+++ src/logging/logger-options.ts
@@ -1,4 +1,5 @@
 import { LogLevelName } from "./log-models";
+import { isLogLevelName } from "./log-models-typeguards";
 
 
 export interface SourceOptions {
@@ -9,10 +10,13 @@ export interface SourceOptions {
  * Options for determining which source and log levels to write to channel output.
  */
 export class LoggerOptions {
+
+
+    private readonly defaultLogLevel: LogLevelName = 'information';
     /**
-     * The minimum log level to write, filtering out everything below it. Default is 'INFORMATION'.
+     * The minimum log level to write, filtering out everything below it. Default is 'information'.
      */
-    public logLevel: LogLevelName = 'information';
+    public logLevel: LogLevelName = this.defaultLogLevel;
 
     /**
      * Exclude or include an array of server log sources.
@@ -23,6 +27,10 @@ export class LoggerOptions {
      * 
      */
     constructor(init?: Partial<LoggerOptions>) {
+        if(init?.logLevel){
+            let logLevelName = init.logLevel.toLowerCase();
+            init.logLevel = isLogLevelName(logLevelName) ? logLevelName : this.defaultLogLevel;
+        }
         Object.assign(this, init);
     }
 
