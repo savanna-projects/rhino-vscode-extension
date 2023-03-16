3ad7d7c64fb81224ec35d0492f500e5dfa17f47a
Gravity API <gravity.api@outlook.com>
2023-03-16 21:25:52 +0200

    updates

--- resources/DemoScript.ps1
+++ resources/DemoScript.ps1
@@ -15,17 +15,35 @@ param(
     [string] $Filter          = $null,
     [string] $Environment     = $null,
     [string] $Driver          = 'MockWebDriver',
+    [string] $ProjectRoot     = [System.IO.Directory]::GetParent($PSScriptRoot),
+    [string] $ReportOut       = [System.IO.Directory]::GetParent($PSScriptRoot),
     [int]    $SearchTimeout   = 15000,
     [int]    $LoadTimeout     = 60000
 )
 #
+# Setup: Functions
+function Get-AsyncStatus {
+    param ([string] $Uri)
+
+    $response = Invoke-WebRequest `
+        -Method Get `
+        -ContentType "application/json" `
+        -Uri $Uri `
+
+    if ($response.StatusCode -ge 400) {
+        Write-Host $response
+        exit 10
+    }
+
+    return ($response.Content | ConvertFrom-Json -Depth 15)
+}
+#
 # Setup: Tests Location (absolute/relative file or folder path)
-$_projectRoot            = [System.IO.Directory]::GetParent($PSScriptRoot)
-$_pluginsRepository      = [System.IO.Path]::Combine($_projectRoot, 'src', 'Plugins')
-$_resourcesRepository    = [System.IO.Path]::Combine($_projectRoot, 'src', 'Resources')
-$_environmentsRepository = [System.IO.Path]::Combine($_projectRoot, 'src', 'Environments')
-$_modelsRepository       = [System.IO.Path]::Combine($_projectRoot, 'src', 'Models')
-$_testsRepository        = [System.IO.Path]::Combine($_projectRoot, 'src', 'Tests')
+$_pluginsRepository      = [System.IO.Path]::Combine($ProjectRoot, 'src', 'Plugins')
+$_resourcesRepository    = [System.IO.Path]::Combine($ProjectRoot, 'src', 'Resources')
+$_environmentsRepository = [System.IO.Path]::Combine($ProjectRoot, 'src', 'Environments')
+$_modelsRepository       = [System.IO.Path]::Combine($ProjectRoot, 'src', 'Models')
+$_testsRepository        = [System.IO.Path]::Combine($ProjectRoot, 'src', 'Tests')
 #
 # Setup: Endpoints
 $_pluginsEndpoint        = "$($Endpoint)/api/v3/plugins"
@@ -33,7 +51,8 @@ $_resourcesEndpoint      = "$($Endpoint)/api/v3/resources/bulk"
 $_environmentsEndpoint   = "$($Endpoint)/api/v3/environment"
 $_modelsJsonEndpoint     = "$($Endpoint)/api/v3/models"
 $_modelsMarkdownEndpoint = "$($Endpoint)/api/v3/models/md"
-$_invocationEndpoint     = "$($Endpoint)/api/v3/rhino/configurations/invoke"
+$_invocationEndpoint     = "$($Endpoint)/api/v3/rhino/async/configurations/invoke"
+$_statusEndpoint         = "$($Endpoint)/api/v3/rhino/async/status"
 #
 # Setup: Constants
 $_newLines  = [System.Environment]::NewLine + [System.Environment]::NewLine
@@ -45,26 +64,28 @@ $content  = (Get-ChildItem -Recurse -Path $_pluginsRepository -File) | ForEach-O
 }
 if($content.Count -gt 0) {
     $body     = [string]::Join($_separator, $content)
+    Write-Debug $body
     $response = Invoke-WebRequest -Uri $_pluginsEndpoint -Body $body -ContentType 'text/plain' -Method Post
     if ($response.StatusCode -ge 400) {
-        Write-Error $response
+        Write-Host $response
         exit 10
     }
 }
 #
 # Register: Resources
-$content = (Get-ChildItem -Recurse -Path $_resourcesRepository -File) | ForEach-Object {
+$content = @((Get-ChildItem -Recurse -Path $_resourcesRepository -File) | ForEach-Object {
     @{
         fileName = [System.IO.Path]::GetFileName($_.Name)
         path = $_.FullName
         content = Get-Content -Encoding utf8 -Path $_.FullName -Raw
     }
-}
+})
 if($content.Count -gt 0) {
     $body     = ConvertTo-Json $content
+    Write-Debug $body
     $response = Invoke-WebRequest -Uri $_resourcesEndpoint -Body $body -ContentType 'application/json' -Method Post
     if ($response.StatusCode -ge 400) {
-        Write-Error $response
+        Write-Host $response
         exit 10
     }
 }
@@ -73,9 +94,10 @@ if($content.Count -gt 0) {
 $content = (Get-ChildItem -Recurse -Path $_environmentsRepository -File -Filter "$($Environment).*") | Select-Object -First 1
 if($content.Count -gt 0) {
     $body     = (Get-Content -Encoding utf8 -Path $content.FullName -Raw)
+    Write-Debug $body
     $response = Invoke-WebRequest -Uri $_environmentsEndpoint -Body $body -ContentType 'application/json' -Method Post
     if ($response.StatusCode -ge 400) {
-        Write-Error $response
+        Write-Host $response
         exit 10
     }
 }
@@ -86,9 +108,10 @@ $content = @((Get-ChildItem -Recurse -Path $_modelsRepository -File -Filter "*.j
 })
 if($content.Count -gt 0) {
     $body     = ConvertTo-Json $content -Depth 3
+    Write-Debug $body
     $response = Invoke-WebRequest -Uri $_modelsJsonEndpoint -Body $body -ContentType 'application/json' -Method Post
     if ($response.StatusCode -ge 400) {
-        Write-Error $response
+        Write-Host $response
         exit 10
     }
 }
@@ -99,9 +122,10 @@ $content = @((Get-ChildItem -Recurse -Path $_modelsRepository -File -Filter "*.r
 })
 if($content.Count -gt 0) {
     $body     = [string]::Join($_separator, $content)
+    Write-Debug $body
     $response = Invoke-WebRequest -Uri $_modelsMarkdownEndpoint -Body $body -ContentType 'text/plain' -Method Post
     if ($response.StatusCode -ge 400) {
-        Write-Error $response
+        Write-Host $response
         exit 10
     }
 }
@@ -126,6 +150,12 @@ $configuration = @{
         elementSearchingTimeout = $SearchTimeout
         pageLoadTimeout         = $LoadTimeout
     }
+    reportConfiguration = @{
+        reporters = @(
+            "JunitRepoter",
+            "BasicReporter"
+        )
+    }
     testsRepository = @((Get-ChildItem -Recurse -Path $_testsRepository -File -Filter "*.rhino") | ForEach-Object { 
         Get-Content -Encoding utf8 -Path $_.FullName -Raw
     })
@@ -133,7 +163,7 @@ $configuration = @{
 }
 #
 # Invoke Configuration
-Write-Host "Invoking configuration on $($rhinoEndpoint), please wait..."
+Write-Host "Invoke-Configuration -Async -Uri $($Endpoint) -Filter $($Filter)"
 $body = ConvertTo-Json $configuration
 $response = Invoke-WebRequest `
     -Method Post `
@@ -143,15 +173,38 @@ $response = Invoke-WebRequest `
 #
 # Error From the Server
 if (($response.StatusCode -ge 400) -or ($response.StatusCode -eq 204)) {
-    Write-Error $response
+    Write-Host $response
     exit 10
 }
 #
-# Assert That All Tests Passed
+# Setup
 $responseObj = ($response.Content | ConvertFrom-Json)
+#
+# Check Run Async Status
+$id          = $responseObj.id
+$asyncStatus = Get-AsyncStatus -Uri "$($_statusEndpoint)/$($id)"
+while ($asyncStatus.status -ne 'Complete') {
+    Start-Sleep -Seconds 30
+    $asyncStatus = Get-AsyncStatus -Uri "$($_statusEndpoint)/$($id)"
+    Write-Host "$(Get-Date -Format "yyyy-MM-dd hh:mm:ss.fff") in progress: $($asyncStatus.progress)%"
+}
+#
+# Get & Save JUnit Report
+$key = $asyncStatus.entityOut.key
+$reportEndpoint = "$($Endpoint)/reports/rhino-$($key)/junit.xml"
+$response = Invoke-WebRequest -Uri $reportEndpoint -Method Get
+if($response.StatusCode -eq 200) {
+    $reportArtifact = [System.IO.Path]::Combine($ReportOut, 'junit.xml')
+    Set-Content -Path $reportArtifact -Value $response.Content
+}
+else {
+    Write-Host "Was not able to create JUnit report from '$($Endpoint)/reports/rhino-$($key)/junit.xml'"
+}
+#
+# Assert That All Tests Passed
 if ($responseObj.actual) {
     Write-Host "All {$($responseObj.testCases.Length)} test(s) passed"
     exit 0
 }
-Write-Host "There are test(s) failure(s); See report"
+Write-Host "There are test(s) failure(s)"
 exit 10
